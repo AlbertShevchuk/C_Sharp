f039e06691f60a540a1a2b964b924cadb890f1cd
Albert <albertshevchuk@gmail.com>
2020-08-16 21:52:50 -0700

    custom data types

---
+++ customDataTypes/customDataTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BasicClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forest f = new Forest("Amazon");
+            Console.WriteLine(f.Trees);
+
+            for (var i = 0; f.age < 5; i++)
+            { f.Grow(); }
+
+            Console.WriteLine(f.Trees);
+            f.Burn();
+
+            Console.WriteLine(f.Trees);
+            Console.WriteLine(f.age);
+        }
+    }
+}
